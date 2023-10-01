d97c38db57275fe47d51cb39dddd09f58d8d7bc0
benev0 <benjaminevanownby@gmail.com>
2023-10-01 19:33:03 -0400

    psql connected

--- Bot.py
+++ Bot.py
@@ -1,5 +1,8 @@
+#import discord.py
 import discord
 from discord.ext import commands
+#import psotgres driver
+import psycopg2
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,7 +12,14 @@ intents.presences = False
 
 client = commands.Bot(command_prefix='$', intents=intents)
 
-allow = {}
+with open("secrets/psqluser") as f:
+    conn = psycopg2.connect(
+        dbname=f.readline().strip(),
+        user=f.readline().strip(),
+        password=f.readline().strip(),
+        host=f.readline().strip(),
+        port=f.readline().strip()
+    )
 
 class NoPrivateMessages(commands.CheckFailure):
     pass
@@ -17,25 +27,31 @@ class NoPrivateMessages(commands.CheckFailure):
 class NotAdmin(commands.CheckFailure):
     pass
 
-def get_expected():
-    pass
-
-def check_username(user):
-    return get_expected(user.id) == user.display_name;
-
+def get_expected(id: int):
+    with conn.cursor() as cur:
+        cur.execute(f"SELECT NAME FROM allowed_names WHERE id = {id};")
+        return cur.fetchone()
 
 @client.event
 async def on_member_update(before, after):
-    if not check_username(after.display_name):
+    expected = get_expected(after.id);
+    print(expected)
+    if expected in after.display_name:
+        print("username ok")
         return
+    print(after.display_name)
+    print(len(after.display_name))
 
 @client.command()
 async def status(ctx):
     await ctx.send('I am online!!')
+    print(type(ctx.author.id))
 
 @client.command()
-async def name(ctx, user, nick):
-    pass
+async def name(ctx, member: discord.Member, nick):
+    print("name recieved")
+    await member.edit(nick=nick)
+    await ctx.send(f"{member} aka {nick}")
 
 
 @client.event
@@ -49,8 +65,7 @@ async def on_message(message):
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-if __name__ == '__main__':
 
-    with open("token") as tFile:
-        token = tFile.readline()
-    client.run(token)
+with open("secrets/token") as tFile:
+    token = tFile.readline()
+client.run(token)
